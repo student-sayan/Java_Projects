037d0f744bd0f9f6a1fa2139c0691715fd53b033
student-sayan <sayanroy702388@gmail.com>
2025-05-08 10:28:31 +0530

    Create PositiveOrNegative.java

---
+++ PositiveOrNegative.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class PositiveOrNegative {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter any number :- ");
+        int number = sc.nextInt();
+
+        if (number > 0){
+            System.out.println("The number is Positive....,");
+        } else if (number < 0) {
+            System.out.println("The number is Negative....,");
+        }
+        else {
+            System.out.println("The number is Zero....,");
+        }
+    }
+}
